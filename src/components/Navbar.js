f68d7bdb1e2ac97ae9079c173df1eb1c687cb255
tofusupreme <yunishbacon@gmail.com>
2022-05-24 08:45:12 +0900

    fixed img logo error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-
+import logo from "../images/logo.png"
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/react-logo.png"alt="" className="nav--icon" />
+        <img src= {logo} alt="" className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
